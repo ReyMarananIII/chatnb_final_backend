d7542e13ec47e659843c7215d07c821bd987ffc7
DevGeraldR <romerogerald22@gmail.com>
2024-03-15 17:13:12 +0800

    Fixed Chat Bug

--- Routes/VisitorRoute.js
+++ Routes/VisitorRoute.js
@@ -74,7 +74,7 @@ router.post("/chat_nb", async (req, res) => {
       model: "gpt-3.5-turbo",
       messages: prompt,
       temperature: 1,
-      max_tokens: 100,
+      max_tokens: 256,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
