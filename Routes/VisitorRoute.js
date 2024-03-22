470c4a165717a7b917c77e2a5e610675fcd84e20
DevGeraldR <romerogerald22@gmail.com>
2024-03-23 00:08:02 +0800

    Fixed TTS Bug

--- Routes/VisitorRoute.js
+++ Routes/VisitorRoute.js
@@ -84,7 +84,7 @@ router.post("/chat_nb", async (req, res) => {
       model: "gpt-3.5-turbo",
       messages: prompt,
       temperature: 1,
-      max_tokens: 256,
+      max_tokens: 100,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
