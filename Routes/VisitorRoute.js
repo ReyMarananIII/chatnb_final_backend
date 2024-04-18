f924c84621034f5a9ef6ff7329177375e31452cb
DevGeraldR <romerogerald22@gmail.com>
2024-04-18 17:26:58 +0800

    Updated Code

--- Routes/VisitorRoute.js
+++ Routes/VisitorRoute.js
@@ -157,16 +157,21 @@ router.post("/chat_nb", async (req, res) => {
     // Transcribe the text response to audio
     const fileName = "Public/Audios/message_audio.mp3";
     const message = completion.choices[0].message.content; // The text to convert to speech
-    await voice.textToSpeech({
-      fileName: fileName, // The name of your audio file
-      textInput: message, // The text you wish to convert to speech
-      voiceId: nb.voiceID, // A Voice ID from Elevenlabs
-      stability: 0.5, // The stability for the converted speech
-      similarityBoost: 0.5, // The similarity boost for the converted speech
-      modelId: "eleven_turbo_v2", // The ElevenLabs Model ID
-      style: 1, // The style exaggeration for the converted speech
-      speakerBoost: true, // The speaker boost for the converted speech
-    });
+
+    //_______________________ELEVENLABS SWITCH_______________________________
+
+    // await voice.textToSpeech({
+    //   fileName: fileName, // The name of your audio file
+    //   textInput: message, // The text you wish to convert to speech
+    //   voiceId: nb.voiceID, // A Voice ID from Elevenlabs
+    //   stability: 0.5, // The stability for the converted speech
+    //   similarityBoost: 0.5, // The similarity boost for the converted speech
+    //   modelId: "eleven_turbo_v2", // The ElevenLabs Model ID
+    //   style: 1, // The style exaggeration for the converted speech
+    //   speakerBoost: true, // The speaker boost for the converted speech
+    // });
+
+    //________________________________________________________________________
 
     // generate lipsync
     await lipSyncMessage();
