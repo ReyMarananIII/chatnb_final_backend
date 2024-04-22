235d2005965be560148fd752d8f26f5f89a7a9f7
DevGeraldR <romerogerald22@gmail.com>
2024-04-22 21:07:23 +0800

    Fixed lipsync

--- Routes/VisitorRoute.js
+++ Routes/VisitorRoute.js
@@ -166,7 +166,7 @@ router.post("/chat_nb", async (req, res) => {
       voiceId: nb.voiceID, // A Voice ID from Elevenlabs
       stability: 0.5, // The stability for the converted speech
       similarityBoost: 0.5, // The similarity boost for the converted speech
-      modelId: "eleven_turbo_v2", // The ElevenLabs Model ID
+      modelId: "eleven_multilingual_v2", // The ElevenLabs Model ID
       style: 1, // The style exaggeration for the converted speech
       speakerBoost: true, // The speaker boost for the converted speech
     });
