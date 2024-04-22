a62b1398b92a8d316fff10ae306cb157ac47018b
DevGeraldR <romerogerald22@gmail.com>
2024-04-23 00:07:26 +0800

    Improved Voice

--- Routes/VisitorRoute.js
+++ Routes/VisitorRoute.js
@@ -165,9 +165,8 @@ router.post("/chat_nb", async (req, res) => {
       textInput: message, // The text you wish to convert to speech
       voiceId: nb.voiceID, // A Voice ID from Elevenlabs
       stability: 0.5, // The stability for the converted speech
-      similarityBoost: 0.5, // The similarity boost for the converted speech
+      similarityBoost: 0.75, // The similarity boost for the converted speech
       modelId: "eleven_multilingual_v2", // The ElevenLabs Model ID
-      style: 1, // The style exaggeration for the converted speech
       speakerBoost: true, // The speaker boost for the converted speech
     });
 
